6dc3042711aaa43e965447d28e31cd3142872e49
AnitaEpsilon <54986195+AnitaEpsilon@users.noreply.github.com>
2024-02-21 13:42:06 +0300

    Initial commit: cookiecutter DS template used

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Test task for solving CAPTCHA by selecting the correct image number according to the image instructions',
+    author='AnitaEpsilon',
+    license='',
+)
